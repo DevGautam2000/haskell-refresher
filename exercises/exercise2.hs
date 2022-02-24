1b67b160c2dcc0883f6cfaf716f478cec310fc37
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-02-24 14:42:50 +0530

    added more solns

--- exercises/exercise2.hs
+++ exercises/exercise2.hs
@@ -5,12 +5,20 @@
 
 -}
 
+-- write a program to check if a number is pallindrome or not 
 
--- Write a function to check if a number is pallindrome or not 
+-- usign function composition
+pallindrome :: Int -> (Bool,String)
+pallindrome num = (show num == reverse (show num),show num)
 
-isPallindrome :: Int -> Bool 
-isPallindrome x = show x == reverse (show x)
+toText :: (Bool,String) -> String
+toText (isPall,num) = 
+    if isPall then num ++ " is pallindrome"
+    else num ++ " is not pallindrome"
 
-
-
+isPallindrome :: Int -> String 
+isPallindrome = toText.pallindrome
     
+main = do
+    print $isPallindrome 232
+    print $isPallindrome 32
